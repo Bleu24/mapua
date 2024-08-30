b902e2d64a10bf125ef57c9e75be35388dd0351a
Bryan Reamillo <bryanreamillo2403@gmail.com>
2024-08-30 16:15:25 +0800

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CourseComponent } from './course/course.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { ResearchComponent } from './research/research.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainBodyComponent,
+    HeaderComponent,
+    FooterComponent,
+    CourseComponent,
+    FacultyComponent,
+    ResearchComponent
   ],
   imports: [
     BrowserModule,
